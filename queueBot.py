8d77054d4a4923f6de0733fe93f0ff052ad6f02a
Martin Sørensen <maros105@gmail.com>
2020-03-20 15:28:03 +0100

    Separate data files for each connected server and minor messaging tweaks

--- queueBot.py
+++ queueBot.py
@@ -1,4 +1,4 @@
-import os, json, discord.utils
+import os, json, discord.utils, pprint
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -7,11 +7,13 @@ separator = '------------------------------------------------------------------'
 bot = commands.Bot(command_prefix='!')
 
 dataFile = 'queueData.json'
-data = {'queue':[]}
+data = {}
 
 def saveToJson(filename, jsonDict):
+    if not os.path.exists('./data'):
+        os.mkdir('./data')
     try:
-        file = open(filename, 'w')
+        file = open('./data/{}.json'.format(filename), 'w')
         file.write(json.dumps(jsonDict))
         file.close()
     except Exception as e:
@@ -21,7 +23,7 @@ def saveToJson(filename, jsonDict):
 def loadFromJson(filename):
     content = None
     try:
-        file = open(filename, 'r')
+        file = open('./data/{}.json'.format(filename), 'r')
         content = json.loads(file.read())
         file.close()
     except Exception as e:
@@ -30,32 +32,31 @@ def loadFromJson(filename):
         raise
     return content
 
-def getScreenName(caller):
-    if caller.nick:
-        return caller.nick
-    else:
-        return caller.name
-
-def printQueue(ctx):
+async def printQueue(ctx):
     global separator
     print(separator)
-    if len(data['queue']) > 0:
-        for i in range(len(data['queue'])):
-            call = data['queue'][i]
-            print(i, call['server'], getScreenName(discord.utils.get(ctx.guild.members, id=call['id'])), call['message'], call['id'])
+    if len(data[ctx.guild.id]['queue']) > 0:
+        queuestring = ''
+        print('{}\n'.format(ctx.guild.name))
+        for i in range(len(data[ctx.guild.id]['queue'])):
+            call = data[ctx.guild.id]['queue'][i]
+            callername = discord.utils.get(ctx.guild.members, id=call['id']).display_name
+            print(i, callername, call['message'])
+            queuestring += '{}:\t{}\n'.format(i, callername)
+        await ctx.send(queuestring)
     else:
         print('Queue empty')
     print(separator)
 
 def addToQueue(caller, server, message):
-    data['queue'].append({'id': caller.id, 'server': server.name, 'message': message})
+    data[server.id]['queue'].append({'id': caller.id, 'server': server.name, 'message': message})
 
-def saveState(ctx):
-    saveToJson(dataFile, data)
-    printQueue(ctx)
+async def saveState(ctx):
+    saveToJson(ctx.guild.id, data[ctx.guild.id])
+    await printQueue(ctx)
 
 def isQueued(caller):
-    for call in data['queue']:
+    for call in data[caller.guild.id]['queue']:
         if caller.id == call['id']:
             return True
     return False
@@ -64,54 +65,58 @@ def isQueued(caller):
 async def call(ctx, message=''):
     if isQueued(ctx.author):
         await ctx.send('You are already queueing. Patience please.')
-    elif ctx.valid:
+    else:
         addToQueue(ctx.author, ctx.guild, message)
-        if len(data['queue']) > 1:
-            await ctx.send('Noted {}. {} queueing in front of you.'.format(ctx.author.display_name, len(data['queue'])-1))
+        if len(data[ctx.guild.id]['queue']) > 1:
+            await ctx.send('Noted {}. {} queueing in front of you.'.format(ctx.author.display_name, len(data[ctx.guild.id]['queue'])-1))
         else:
             await ctx.send('Noted. {} is next up.'.format(ctx.author.display_name))
-        await ctx.guild.owner.send('{} calls in queue'.format(len(data['queue'])))
-        saveState(ctx)
-    else:
-        print('Something went wrong in call()')
+        await ctx.send('{} in queue for you {}'.format(len(data[ctx.guild.id]['queue']), guild.owner.mention))
+        await saveState(ctx)
 
 @bot.command(name='nvm', help='Never mind. The equivalent of lowering a raied hand')
 async def nvm(ctx):
-    for i in range(len(data['queue'])):
-        if data['queue'][i]['id'] == ctx.author.id:
-            data['queue'].pop(i)
+    for i in range(len(data[ctx.guild.id]['queue'])):
+        if data[ctx.guild.id]['queue'][i]['id'] == ctx.author.id:
+            data[ctx.guild.id]['queue'].pop(i)
             ctx.send('{} removed from queue'.format(ctx.author.display_name))
 
 @bot.command(name='next', help='Teachers only. Get the next question or comment in the queue.')
 async def next(ctx):
-    if len(data['queue']) > 0 and ctx.author == ctx.guild.owner:
-        call = data['queue'][0]
+    if len(data[ctx.guild.id]['queue']) > 0 and ctx.author == ctx.guild.owner:
+        call = data[ctx.guild.id]['queue'][0]
         caller = discord.utils.get(ctx.guild.members, id=call['id'])
-        await ctx.send('Next up: {}'.format(caller.display_name))
-        await ctx.guild.owner.send('Next up: {} {} {}'.format(call['server'], caller.display_name, call['message']))
-        await caller.send('You are up!')
-        data['queue'].pop(0)
-        saveState(ctx)
-        if caller.voice.channel:
-            await ctx.guild.owner.move_to(caller.voice.channel)
+        await ctx.send('You are up {}!'.format(caller.mention))
+        await ctx.guild.owner.send('Next up: {} {}'.format(caller.display_name, call['message']))
+        data[ctx.guild.id]['queue'].pop(0)
+        await saveState(ctx)
+        #if caller.voice.channel:
+        #    await ctx.guild.owner.move_to(caller.voice.channel)
         #https://discordpy.readthedocs.io/en/latest/api.html?highlight=move%20member#discord.Member.move_to
-    await ctx.send('{} queueing'.format(len(data['queue'])))
+    await ctx.send('{} queueing'.format(len(data[ctx.guild.id]['queue'])))
 
 @bot.command(name='clear', help='Teachers only. Clears the queue.')
 async def clear(ctx):
     if ctx.author == ctx.guild.owner:
-        data['queue'] = []
-        saveState(ctx)
+        data[ctx.guild.id]['queue'] = []
+        await saveState(ctx)
 
 @bot.event
 async def on_ready():
-    global separator
-    print('\n{} online in:'.format(bot.user.name))
+    global separator, data
     for guild in bot.guilds:
-        print(guild.name)
+        print('\n{} online in {}:'.format(bot.user.name, guild.name))
+        print('Loading data')
+        try:
+            data[guild.id] = loadFromJson(guild.id)
+        except FileNotFoundError:
+            print('No saved data found. Creating data file.')
+            data[guild.id] = {'serverID':guild.id, 'servername':guild.name, 'queue':[]}
+            saveToJson(guild.id, data[guild.id])
         for channel in guild.text_channels:
             print(channel.name)
             #await channel.send('Qbot is online for your queueing pleasure')
+    pprint.pprint(data)
 
 @bot.event
 async def on_member_join(member):
@@ -121,13 +126,6 @@ def main():
     global data
     print('Setting up')
     load_dotenv()
-    print('\nLoading data')
-    try:
-        data = loadFromJson(dataFile)
-    except FileNotFoundError:
-        print('No saved data found. Creating data file.')
-        saveToJson(dataFile, data)
-
     print('\nRunning bot.\nAdd the bot to servers with this link:\n{}'.format(os.getenv('BOT_LINK')))
     bot.run(os.getenv('DISCORD_TOKEN'))
 
